7441167d4019d2da2f186c1b18d0d5aae095ef1c
wrymaszewski <plantpathibb@gmail.comgit config --global user.email plantpathibb@gmail.comgit config --global user.email plantpathibb@gmail.com>
2018-05-10 13:55:11 +0200

    amazon conf6

--- veturilo/settings.py
+++ veturilo/settings.py
@@ -23,7 +23,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'o$+rk-tt-r%u4_8jptpq(q86_bpl5@899ddq9r4im_zj1xmpn9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['eb-django-app-dev.elasticbeanstalk.com']
 
