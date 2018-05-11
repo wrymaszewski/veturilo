e54cca170e1acadc528bc8471357516bc28616cc
wrymaszewski <plantpathibb@gmail.comgit config --global user.email plantpathibb@gmail.comgit config --global user.email plantpathibb@gmail.com>
2018-05-11 14:57:28 +0200

    celery fixes2

--- veturilo/_celery.py
+++ veturilo/_celery.py
@@ -1,14 +1,17 @@
-# coding=utf-8
-
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from django.conf import settings
 
-# Indicate Celery to use the default Django settings module
+# set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'veturilo.settings')
 
 app = Celery('veturilo')
-app.config_from_object('django.conf:settings')
 
-# This line will tell Celery to autodiscover all your tasks.py that are in your app folders
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+# Using a string here means the worker don't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
--- veturilo/settings.py
+++ veturilo/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'kombu.transport.django',
+    # 'kombu.transport.django',
     'location_field.apps.DefaultConfig',
     'djcelery',
     'scraper',
